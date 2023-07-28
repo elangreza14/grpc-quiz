baf127b23ef0b768d9ab61fe970ca08a2824078f
elangreza14 <rezaelangerlangga14@gmail.com>
2023-07-29 00:47:43 +0700

    refactor expected result

--- internal/usecase/gameplay.go
+++ internal/usecase/gameplay.go
@@ -17,14 +17,14 @@ type (
 
 	// GamePlay is ...
 	GamePlay struct {
-		players          map[string]int
-		state            State
-		internalStream   chan *GameState
-		externalStream   chan string
-		questions        map[string]bool
-		timePerRound     time.Duration
-		expectedQuestion string
-		expectedAnswer   bool
+		players        map[string]int
+		state          State
+		internalStream chan *GameState
+		externalStream chan string
+		questionStream chan *QuestionPayload
+		questions      map[string]bool
+		timePerRound   time.Duration
+		expected       QuestionPayload
 	}
 
 	// SubmitAnswerPayload ...
@@ -41,14 +41,12 @@ type (
 )
 
 const (
-	// Waiting is state when waiting all the players
-	Waiting State = iota
-	// Started is state when game is started
-	Started
+	// Wait is state when waiting all the players
+	Wait State = iota
+	// Start is state when game is started
+	Start
 	// SetQuestion is state when game is started
 	SetQuestion
-	// Announcement is state when game is started
-	Announcement
 	//  AnswerQuestion is state when game is finished
 	AnswerQuestion
 	//  Finish is state when game is finished
@@ -62,43 +60,41 @@ func NewGamePlay() *GamePlay {
 		"1 - 1 = -1": false,
 		"1 * 0 = 0":  true,
 	}
+
 	g := &GamePlay{
-		players:          map[string]int{},
-		state:            Waiting,
-		internalStream:   make(chan *GameState),
-		externalStream:   make(chan string),
-		questions:        Questions,
-		timePerRound:     3 * time.Second,
-		expectedQuestion: "",
-		expectedAnswer:   true,
+		players:        map[string]int{},
+		state:          Wait,
+		internalStream: make(chan *GameState),
+		externalStream: make(chan string),
+		questionStream: make(chan *QuestionPayload, len(Questions)),
+		questions:      Questions,
+		timePerRound:   3 * time.Second,
 	}
+
 	go g.listenInternalStream()
 	go g.listenQuestion()
 
 	return g
 }
 
-// Start ...
-func (g *GamePlay) Start() {
-	g.internalStream <- &GameState{
-		State: Started,
-	}
-}
-
 func (g *GamePlay) listenInternalStream() {
 	for res := range g.internalStream {
 		switch res.State {
-		case Started:
+		case Start:
 			g.externalStream <- "game started"
-			g.state = Started
+			g.state = Start
+			for question, answer := range g.questions {
+				g.questionStream <- &QuestionPayload{
+					question: question,
+					answer:   answer,
+				}
+			}
 		case SetQuestion:
-			payload := res.Payload.(QuestionPayload)
-			g.expectedAnswer = payload.answer
-			g.expectedQuestion = payload.question
-			g.externalStream <- g.expectedQuestion
+			g.expected = res.Payload.(QuestionPayload)
+			g.externalStream <- g.expected.question
 		case AnswerQuestion:
 			payload := res.Payload.(SubmitAnswerPayload)
-			if payload.answer == g.expectedAnswer {
+			if payload.answer == g.expected.answer {
 				g.players[payload.name]++
 			}
 		case Finish:
@@ -108,9 +104,33 @@ func (g *GamePlay) listenInternalStream() {
 	}
 }
 
+// Start ...
+func (g *GamePlay) Start() {
+	g.internalStream <- &GameState{
+		State: Start,
+	}
+}
+
+func (g *GamePlay) listenQuestion() {
+	for i := 0; i < len(g.questions); i++ {
+		v := <-g.questionStream
+
+		g.internalStream <- &GameState{
+			State:   SetQuestion,
+			Payload: *v,
+		}
+
+		time.Sleep(g.timePerRound)
+	}
+
+	g.internalStream <- &GameState{
+		State: Finish,
+	}
+}
+
 // SubmitAnswer ...
 func (g *GamePlay) SubmitAnswer(name string, answer bool) {
-	if g.state != Started {
+	if g.state != Start {
 		return
 	}
 
@@ -129,36 +149,12 @@ func (g *GamePlay) SubmitAnswer(name string, answer bool) {
 }
 
 // AddPlayer ...
-func (g *GamePlay) AddPlayer(name string) {
-	g.players[name] = 0
-}
+func (g *GamePlay) AddPlayer(name string) { g.players[name] = 0 }
 
 // ListenStream ...
-func (g *GamePlay) ListenStream() <-chan string {
-	return g.externalStream
-}
+func (g *GamePlay) ListenStream() <-chan string { return g.externalStream }
 
-func (g *GamePlay) listenQuestion() {
-	for {
-		if g.state != Started {
-			continue
-		}
-
-		for i, v := range g.questions {
-			g.internalStream <- &GameState{
-				State: SetQuestion,
-				Payload: QuestionPayload{
-					question: i,
-					answer:   v,
-				},
-			}
-
-			time.Sleep(g.timePerRound)
-		}
-
-		g.internalStream <- &GameState{
-			State: Finish,
-		}
-		return
-	}
+// GetStats ...
+func (g *GamePlay) GetStats() string {
+	return "g.externalStream"
 }
